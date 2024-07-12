891f6961f45504ba8dbabc911d330658b5d725b2
Mohamed Faaris <mohammedfaaris2005@gmail.com>
2024-07-12 15:12:59 +0530

    script completed

--- script.js
+++ script.js
@@ -1,19 +1,76 @@
 
+function remove_character_at_index(str, index) {
+    if (index < 0 || index >= str.length) {
+        return str;
+    }
+    return str.slice(0, index) + str.slice(index + 1);
+}
+
 function no_of_unique_characters(name_1,name_2)
 {
     for(var i = 0;i<name_1.length;i++)
     {
         var character = name_1.charAt(i);
         if (name_2.indexOf(character) != -1)
-        {
-            console.log(i);   
+        {   
             name_1 = name_1.replace(character,'');
             name_2 = name_2.replace(character,'');
             i--;
         }
     }
-    console.log(name_1+name_2+" - infunction");
+    console.log(name_1+'  '+name_2+" - infunction");
     return (name_1+name_2).length;
+}
+
+function calculate_flames_by_number(str,number,start)
+{
+    console.log(str,number,start);
+    if(str.length<2)
+    {
+        return str;
+    }
+    var index = (number+start)%str.length;
+    str = remove_character_at_index(str,index);
+    start = (str.length+index-1)%str.length;
+    return calculate_flames_by_number(str,number,start);
+}
+
+function relation(character){
+    if(character == "f")
+        {
+            return "friends";
+        }
+
+    else if(character == "l")
+        {
+            return "love";
+        }
+
+    else if(character == "a")
+        {
+            return "affection";
+        }
+        
+    else if(character == "m")
+        {
+            return "mariage";
+        }
+        
+    else if(character == "e")
+        {
+            return "enemy";
+        }
+    
+    else if(character == "s")
+        {
+            return "friends";
+        }
+    
+    else
+    {
+        console.log("relation: ",character);
+        return "invalid";
+    }
 }
 
 function calculate_flames(event)
@@ -22,8 +79,6 @@ function calculate_flames(event)
     var result = document.getElementById('result');
     var name_1 = document.getElementById('name_1').value.toLowerCase().replace(/\s+/g, '');
     var name_2 = document.getElementById('name_2').value.toLowerCase().replace(/\s+/g, '');
-    console.log(name_1+name_2+" beforefunction");
     var number = no_of_unique_characters(name_1,name_2);
-    console.log(number+" afterfunction");
-    result.innerText = "result: "+number.toString();
+    result.innerText = "result: "+relation(calculate_flames_by_number("flames",number,0));
 }
